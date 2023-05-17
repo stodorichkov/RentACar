3a60c6b6e0e11342cb989823af5cdaa9c7055b0f
StelianRBG <steliantodorichkov03@gmail.com>
2023-05-17 03:04:30 +0300

    Create Sign In and Sign Up forms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,29 @@
 import './App.css';
-import Bar from './components/Bar';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
-import { Avatar } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { BrowserRouter as Router ,Routes, Route, Navigate } from 'react-router-dom';
+
+import Bar from './components/Bar';
+import SignInForm from './components/SignInForm';
+import SignUpForm from './components/SIgnUpForm';
 
 const App = () => {
 	return (
 		<>
-			<ThemeProvider theme={theme}>
-				<Bar />
-			</ThemeProvider>
+			<Router>
+				<CssBaseline />
+				<ThemeProvider theme={theme}>
+					<Bar />
+					<Routes>
+						<Route path='/'/>
+						<Route path='/signin' element={<SignInForm />}/>
+						<Route path='/signup' element={<SignUpForm />}/>
+						<Route path="*" element={<Navigate to="/" />} />
+					</Routes>
+				</ThemeProvider>  
+			</Router>
 		</>
 	);
 }
--- client/src/components/Bar.jsx
+++ client/src/components/Bar.jsx
@@ -1,23 +1,23 @@
-import { AppBar, Toolbar, Button, Stack, Typography, Grid, Avatar, IconButton }  from '@mui/material';
+import { AppBar, Toolbar, Button, Stack, Typography, Grid, Avatar}  from '@mui/material';
 
 const Bar = () => {
 	return (
 		<>
-			<AppBar position="fixed" color="background">
+			<AppBar position="absolute" color="background" sx={{padding: '1rem'}}>
 				<Toolbar>
 					<Grid container direction="row" justifyContent="space-between">
-						<Button>
-							<Avatar alt="Remy Sharp" src="logo.png" />
-							<Typography variant="h4" color="white" sx={{textTransform: "none"}}>
+						<Button sx={{textTransform: "none"}} href="/">
+							<Avatar alt="Remy Sharp" src="logo.png" sx={{ width: 45, height: 45 }}/>
+							<Typography variant="h4" color="white">
 								odexio Cruisers
 							</Typography>
 						</Button>
 						
 						<Stack spacing={2} direction="row">
-							<Button variant="contained"  color="button_primary" size="large">
+							<Button variant="contained"  color="button_primary" size="large" href="/signin">
 								Sign In
 							</Button>
-							<Button variant="contained"  color="button_secondary" size="large">
+							<Button variant="contained"  color="button_secondary" size="large" href="/signup">
 								Sign Up
 							</Button>
 						</Stack>
