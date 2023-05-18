0676147af504e6e5206e276ae020b3e11bf0774f
NedkoIvanov <nedkiivanov@gmail.com>
2023-05-18 23:29:10 +0300

    adding AuthenticationManagerBuilder :)

--- backend/src/main/java/com/example/demo/config/ApplicationSecurityConfiguration.java
+++ backend/src/main/java/com/example/demo/config/ApplicationSecurityConfiguration.java
@@ -1,18 +1,30 @@
 package com.example.demo.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableMethodSecurity
 public class ApplicationSecurityConfiguration{
+
+    private final UserDetailsService userDetailsService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfiguration(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -45,6 +57,12 @@ public class ApplicationSecurityConfiguration{
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID");
         return http.build();
+    }
+
+
+    @Autowired
+    void registerProvider(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
 }
