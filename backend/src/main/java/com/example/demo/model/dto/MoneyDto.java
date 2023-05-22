93ba1ae7465e0597150a04d770ecf4647f3cf595
niksata998 <nkrivulev7@gmail.com>
2023-05-22 11:29:46 +0300

    unversioned files

---
+++ backend/src/main/java/com/example/demo/model/dto/MoneyDto.java
@@ -0,0 +1,13 @@
+package com.example.demo.model.dto;
+
+public class MoneyDto {
+    private Double money;
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+}
