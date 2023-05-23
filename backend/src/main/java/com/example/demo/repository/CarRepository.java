cb096dc95bafc88c53ea2f652559470c05dc21f2
niksata998 <nkrivulev7@gmail.com>
2023-05-23 16:15:10 +0300

    some changes in the repository

--- backend/src/main/java/com/example/demo/repository/CarRepository.java
+++ backend/src/main/java/com/example/demo/repository/CarRepository.java
@@ -1,8 +1,13 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.CarEntity;
+import com.example.demo.model.dto.CarDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +16,7 @@ public interface CarRepository extends JpaRepository<CarEntity, Long> {
    List<CarEntity> findByIsRented(Boolean isRented);
 
    Optional<CarEntity> findByRegistrationPlate(String registrationPlate);
+
+   @Query("SELECT c FROM CarEntity c LEFT JOIN c.carRental r WHERE :reservationDate NOT BETWEEN r.startTime AND r.endTime")
+   List<CarEntity> findAvailableCarsByReservationDate(@Param("reservationDate") LocalDate reservationDate);
 }
