56bbdb3c45b9fe3dd8351f0121693254294fd7aa
StelianRBG <steliantodorichkov03@gmail.com>
2023-05-26 16:33:43 +0300

    Show user info and add budget

--- client/src/components/Profile/UserInfo.jsx
+++ client/src/components/Profile/UserInfo.jsx
@@ -1,12 +1,16 @@
 import { Paper, TextField, Divider, Button, Avatar, InputAdornment, Stack }  from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTheme } from '@mui/material/styles';
+import { useSelector } from 'react-redux';
 
 import axios from 'axios';
 
 const UserInfo = () => {
+    const user = useSelector((state) => state.user);
+    const theme = useTheme();
+
     const [username, setUsername] = useState('');
     const [age, setAge] = useState('');
     const [email, setEmail] = useState('');
@@ -15,7 +19,6 @@ const UserInfo = () => {
     const [transfer, setTransfer] = useState(0.00);
     const [editMode, setEditMode] = useState(false);
     const [addMoneyMode, setAddMoneyMode] = useState(false);
-    const theme = useTheme();
 
     const handleChangeUsername = (event) => {
         setUsername(event.target.value);
@@ -40,11 +43,42 @@ const UserInfo = () => {
         setTransfer(0.00);
     }
 
-    const transferMoney = () => {
-        setBudget(budget + transfer)
-        switchAddMoneyMode()
-        console.log(budget)
+    axios.defaults.headers.common['Authorization'] = `Bearer ${user.token}`;
+
+    const transferMoney = async() => {
+        try {
+            const response = await axios.put('http://localhost:8086/user/add-money', {money: transfer});
+            if (response.status === 200) {
+                console.log(response.data)
+                setBudget(budget + transfer)
+                switchAddMoneyMode()
+            }
+        }
+        catch (error) {
+            console.error('Error fetching data:', error);
+        } 
     }
+
+    const getUserInfo = async () => {
+        try {
+            const response = await axios.get('http://localhost:8086/user/profile');
+            if (response.status === 200) {
+                setUsername(response.data.username ? response.data.username : '');
+                setAge(response.data.age ? response.data.age : 18);
+                setEmail(response.data.email ? response.data.email : '');
+                setPhone(response.data.phone ? response.data.phone : '');
+                setBudget(response.data.budget ? response.data.budget : 0.00);
+            }
+        }
+        catch (error) {
+            console.error('Error fetching data:', error);
+        } 
+    }
+
+    useEffect(() => {
+        getUserInfo();
+    }, [])
+
 
     return(
         <Grid xl={3.5} lg={4} md={5} sm={7} xs={10}>
